05772d3ee0fe6418b0a99e5dfd1c6b807bc82049
JackieC1993 <jackiecheung@pursuit.org>
2023-06-02 00:24:45 -0400

    install cyberhack, helper.js and index.test.js

---
+++ data/cyberhack.js
@@ -0,0 +1,17 @@
+const { faker } = require("@faker-js/faker")
+const {generateRandomMessage} = require('./index');
+
+
+
+const hacker = {
+    _id: faker.datatype.uuid(),
+    hackerName: ${faker.adjective + Boron + Byson},
+    messageTitle: ${hacker.adjective + hacker.abbreviation on git.branch
+},
+
+    messageContent: lorem.paragraph,
+    isResolved: true || false 
+    messageResponse: hacker.phrase. -date: "the message was posted", 
+    numberOfUpvotes: 800165,
+
+};
