920cd11e72643566a179d07ab27a85a0e7dbab16
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-06-04 16:27:05 -0400

    Finish 'index.js'. Create some products by running 'index.js' with 'create' script.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const { randomProductFactory, createRandomProduct } = require('./src/products');
+const { writeJSONFile, readJSONFile } = require('./src/helpers');
 
+function run() {
+	let products = readJSONFile('./data', 'products.json');
+	console.log(products);
+	if (process.argv[3]) {
+		products.push(...randomProductFactory(process.argv[3]));
+	} else {
+		products.push(createRandomProduct());
+	}
+	writeJSONFile('./data', 'products.json', products);
+}
+
+run();
