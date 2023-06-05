f99e3f4683c92742327513fcec4f4a033030ad7a
Iris Beckham <irisdancer3@gmail.com>
2023-06-05 19:35:37 -0400

    run file: node index.js <number-of-customers>

--- src/index.js
+++ src/index.js
@@ -6,3 +6,22 @@ const {
 
 //how many messages
 
+// Through the command line, a user should be able to choose whether to create one or many randomly generated messages.
+
+
+// The messages should be read from a JSON file.
+
+
+// Additional messages should be able to be added to the JSON file.
+
+function run(){
+    let customersData = readJSONFile("../data", "customers.json");
+    if(process.argv[2]>0){
+        for(let i = 0; i < process.argv[2]; i++){
+            customersData.push(createRandomCustomer());
+        }
+    }
+    writeJSONFile("../data", "customers.json", customersData);
+}
+
+run();
