e9d5922482314665d70861ef60da49ee5f34a676
JackieC1993 <jackiecheung@pursuit.org>
2023-06-04 22:50:45 -0400

    barely understand this lab

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const {generateRandomDestinations, writeDestinationsToFile} = require('../data/dataGenerator');
+
+function bookTrip(){
+    //load the destination from the file
+    const destinations = require('../data/destinations.json'); 
+    
+
+    //random selecting a destination
+    const randomIndex = Math.floor(Math.random() * destinations.length);
+    const destination = destinations[randomIndex];
+
+    if (destination) {
+        console.log(`Booking a trip to ${destination.name}`)
+    } else {
+        console.log(`No trips are available at this moment`)
+    }   
+}
+
+// to generate random destination
+//const tripRandom = generateRandomDestinations();
+    
+// to file the destination
+//writeDestinationsToFile(tripRandom,'../data/destinations.json');
+
+bookTrip()
+
+module.exports = {bookTrip}
