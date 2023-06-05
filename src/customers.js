ed45ff634fee476828d1aec047be923165a5eb11
Iris Beckham <irisdancer3@gmail.com>
2023-06-05 19:19:03 -0400

    createRandomCustomer function

---
+++ src/customers.js
@@ -0,0 +1,12 @@
+const {faker} = require("@faker-js/faker") 
+
+function createRandomCustomer(){
+    const newCustomer = {
+        id: faker.datatype.uuid(), 
+        name: faker.name.fullName(), 
+        memberNumber: faker.phone.number(), 
+        dateOfLastVisit: faker.date.past(3)
+    };
+    return newCustomer;
+}
+console.log(createRandomCustomer())
