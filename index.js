2fc36fc5ce6ffa152cb8a08ef8f828ce8e8a5d7b
Kenneth-Parker <kennethkavon@gmail.com>
2023-06-11 22:25:59 -0400

    hacker forum and utilities added

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const { randomFakeProduct } = require("./products")
+const { createHackerForum, randomCreateHackerForum } = require("./hackerForum");
+const { writeJSONFile, readJSONFile } = require("./helpers");
+
+const run = () => {
+  let hackerForums = readJSONFile("./data", "hackerForums.json");
+  console.log(hackerForums);
+  (process.argv[3]) ? 
+  hackerForums.push(...randomCreateHackerForum(process.argv[3])) : 
+  hackerForums.push(createHackerForum());
+  writeJSONFile("./data", "hackerForums.json", hackerForums);
+}
+
+run();
